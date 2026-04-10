beedd5fd48c50dbc9114f700100e4a9e731c7c55
eptinyco <it.service@eptinyco.tech>
2026-04-11 00:16:25 +0200

    Update to populate All Company Group
    Update to add all Users to the All Company Group

--- groups.tf
+++ groups.tf
@@ -17,6 +17,15 @@ resource "azuread_group_member" "dept_memberships" {
 
 }
 
+# Membership: place each user in their department group
+resource "azuread_group_member" "allCompany_membership" {
+  for_each = local.users_by_upn
+
+  group_object_id  = "0358259c-26d4-4326-999a-e95a2ef8fb99"
+  member_object_id = local.user_object_ids[each.key]
+
+}
+
 # Manual extra groups from groups.csv
 resource "azuread_group" "manual_groups" {
   for_each = local.manual_groups
